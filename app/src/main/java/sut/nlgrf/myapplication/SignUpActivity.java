e6dc4edc5a96343b15dc479273b34d48907e6241
NLGRF <nongrace6654@gmail.com>
2016-09-21 14:27:03 +0700

    Log.d

--- app/src/main/java/sut/nlgrf/myapplication/SignUpActivity.java
+++ app/src/main/java/sut/nlgrf/myapplication/SignUpActivity.java
@@ -1,9 +1,12 @@
 package sut.nlgrf.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -13,6 +16,7 @@ public class SignUpActivity extends AppCompatActivity {
     private String nameString, addressString, phoneString, userString, passwordString, genderString,
             imageString;
     private RadioButton maleRadioButton, femaleRadioButton;
+    private ImageView imageView;
 
 
 
@@ -29,10 +33,33 @@ public class SignUpActivity extends AppCompatActivity {
         userEditText = (EditText) findViewById(R.id.editText4);
         passwordEditText = (EditText) findViewById(R.id.editText5);
         maleRadioButton = (RadioButton) findViewById(R.id.radioButton);
-        femaleRadioButton = findViewById(R.id.radioButton2);
+        femaleRadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
+        //ImageView Controller
+        imageView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent,"โปรดเลือกรูปภาพ"),1);
+
+            } //onClick
+        });
 
     }   //Main Method
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ((requestCode == 1) && (requestCode == RESULT_OK)) {
+            Log.d("MyApplication", "Result ==> Success");
+
+
+        } // if
+    } // onActivityResult
 
     public void clickSignUpSign(View view) {
 
