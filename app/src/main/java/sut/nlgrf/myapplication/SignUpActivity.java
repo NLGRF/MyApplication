dafa53c94b10b7d1f4dd70b6ff71882f7fddc69b
NLGRF <nongrace6654@gmail.com>
2016-09-22 12:01:02 +0700

    BA

--- app/src/main/java/sut/nlgrf/myapplication/SignUpActivity.java
+++ app/src/main/java/sut/nlgrf/myapplication/SignUpActivity.java
@@ -1,27 +1,42 @@
 package sut.nlgrf.myapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Bundle;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import org.jibble.simpleftp.SimpleFTP;
+
+import java.io.File;
+
+import sut.nlgrf.myapplication.MyAlert;
+import sut.nlgrf.myapplication.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
     //Explicit การประกาศตัวแปร
-    private EditText nameEditText, addressEditText, phoneEditText, userEditText, passwordEditText;
-    private String nameString, addressString, phoneString, userString, passwordString, genderString,
-            imageString, imagePathString, imageNameString;
+    private EditText nameEditText, addressEditText,
+            phoneEditText, userEditText, passwordEditText;
+    private String nameString, addressString, phoneString,
+            userString, passwordString, genderString, imageString,
+            imagePathString, imageNameString;
     private RadioButton maleRadioButton, femaleRadioButton;
     private ImageView imageView;
+    private boolean statusABoolean = true;
+    private RadioGroup radioGroup;
 
 
 
@@ -29,7 +44,6 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
 
         //Bind Widget
         nameEditText = (EditText) findViewById(R.id.editText);
@@ -40,53 +54,82 @@ public class SignUpActivity extends AppCompatActivity {
         maleRadioButton = (RadioButton) findViewById(R.id.radioButton);
         femaleRadioButton = (RadioButton) findViewById(R.id.radioButton2);
         imageView = (ImageView) findViewById(R.id.imageView);
+        radioGroup = (RadioGroup) findViewById(R.id.ragGender);
 
         //ImageView Controller
-        imageView.setOnClickListener(new View.OnClickListener(){
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
 
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.setType("image/*");
-                startActivityForResult(Intent.createChooser(intent,"โปรดเลือกรูปภาพ"),1);
+                startActivityForResult(Intent.createChooser(intent, "โปรดเลือกรูปภาพ"), 1);
 
-            } //onClick
+            }   // onClick
         });
 
-    }   //Main Method
+        //Radio Controller
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                switch (i) {
+
+                    case R.id.radioButton:
+                        genderString = "Male";
+                        break;
+                    case R.id.radioButton2:
+                        genderString = "Female";
+                        break;
+                }
+
+            }   // onCheck
+        });
+
+
+
+    }   // Main Method
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if ((requestCode == 1) && (resultCode == RESULT_OK)) {
+
             Log.d("MyApplication", "Result ==> Success");
 
             //Find Path of Image
             Uri uri = data.getData();
-            imagePathString = MyFindPath(uri);
-            Log.d("MyApplication", "imagePathString ==>" + imagePathString);
+            imagePathString = myFindPath(uri);
+            Log.d("MyApplication", "imagePathString ==> " + imagePathString);
 
             //Setup ImageView
             try {
 
-                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver()
+                        .openInputStream(uri));
                 imageView.setImageBitmap(bitmap);
 
-            }catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
 
-        } // if
-    } // onActivityResult
+            statusABoolean = false;
 
-    private String MyFindPath(Uri uri) {
+            imageNameString = imagePathString.substring(imagePathString.lastIndexOf("/"));
+            Log.d("MyApplication","imageNameString ==> " + imageNameString);
+
+        }   // if
+
+    }   // onActivityResult
+
+    private String myFindPath(Uri uri) {
 
         String strResult = null;
 
         String[] strings = {MediaStore.Images.Media.DATA};
-
         Cursor cursor = getContentResolver().query(uri, strings, null, null, null);
+
         if (cursor != null) {
 
             cursor.moveToFirst();
@@ -94,10 +137,9 @@ public class SignUpActivity extends AppCompatActivity {
             strResult = cursor.getString(index);
 
         } else {
-
             strResult = uri.getPath();
-
         }
+
 
 
         return strResult;
@@ -105,29 +147,98 @@ public class SignUpActivity extends AppCompatActivity {
 
     public void clickSignUpSign(View view) {
 
-        //Get Value From Edit Text
-        nameString = nameEditText.getText().toString().trim(); //trim ตัดช่องว่าง หน้า/หลัง String
+        // Get Value From Edit Text
+        nameString = nameEditText.getText().toString().trim();
         addressString = addressEditText.getText().toString().trim();
         phoneString = phoneEditText.getText().toString().trim();
         userString = userEditText.getText().toString().trim();
         passwordString = passwordEditText.getText().toString().trim();
 
         //Check Space
-        if (nameString.equals("") || addressString.equals("") || phoneString.equals("") ||
-                userString.equals("") || passwordString.equals("")) {
+        if (nameString.equals("") || addressString.equals("") ||
+                phoneString.equals("") || userString.equals("") || passwordString.equals("")) {
             //Have Space
-            MyAlert myAlert = new MyAlert(this, R.drawable.rat48,"มีช่องว่าง","กรุณาใส่ข้อมูลให้ครบ");
+            MyAlert myAlert = new MyAlert(this, R.drawable.rat48,
+                    "มีช่องว่าง", "กรุณากรอกทุกช่อง คะ");
             myAlert.myDialog();
-
         } else if (!(maleRadioButton.isChecked() || femaleRadioButton.isChecked())) {
             //Non Check
-            MyAlert myAlert = new MyAlert(this, R.drawable.rat48,"ยังไม่เลือก Gender","กรุณาเลือก Gender");
+            MyAlert myAlert = new MyAlert(this, R.drawable.rat48,
+                    "ยังไม่เลือก Gender", "กรุณาเลือก Gender");
             myAlert.myDialog();
+        } else if (statusABoolean) {
+            //Non Choose Image
+            MyAlert myAlert = new MyAlert(this, R.drawable.rat48,
+                    "ยังไม่เลือก รูปภาพ", "กรุณาเลือกรูปภาพด้วยคะ");
+            myAlert.myDialog();
+
+        } else {
+
+            confirmData();
+
         }
 
 
+    }   // clickSign
 
-    }   //clickSign
+    private void confirmData() {
 
-}   //Main Class
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.rat48);
+        builder.setTitle("โปรดตรวจทานข้อมูล");
+        builder.setMessage("ชื่อ = " + nameString + "\n" +
+                "ที่อยู่ = " + addressString + "\n" +
+                "Phone = " + phoneString + "\n" +
+                "Gender = " + genderString);
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                upLoadImageToServer();
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+
+
+    }   // confirmData
+
+    private void upLoadImageToServer() {
+
+        //Setup New Policy
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        //upLoadImage by FTP
+        try {
+
+            SimpleFTP simpleFTP = new SimpleFTP();
+            simpleFTP.connect("ftp.swiftcodingthai.com",21,"Sut@swiftcodingthai.com","Abc12345");
+            simpleFTP.bin();
+            simpleFTP.cwd("Image");
+            simpleFTP.stor(new File(imagePathString));
+            simpleFTP.disconnect();
+
+            Log.d("MyApplication","Upload Finish");
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    } // upLoadToServer
+
+
+}   // Main Class
+
+
 
